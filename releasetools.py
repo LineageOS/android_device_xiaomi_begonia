e6dadd734e8edd38a4435bfed6895a2ff24c528f
Aayush Gupta <aayushgupta219@gmail.com>
2020-12-11 14:53:15 +0530

    begonia: Pack required firmware into the final lineage build
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: If3b904dd789b7b597695f2282267f87d378135b1

--- releasetools.py
+++ releasetools.py
@@ -19,24 +19,37 @@ import common
 
 def FullOTA_InstallEnd(info):
     OTA_InstallEnd(info, False)
+    Firmware_Images(info, False)
 
 
 def IncrementalOTA_InstallEnd(info):
     OTA_InstallEnd(info, True)
+    Firmware_Images(info, True)
 
 
-def AddImage(info, basename, dest, incremental):
+def AddImage(info, basename, dest, incremental, firmware):
     name = basename
     if incremental:
         input_zip = info.source_zip
     else:
         input_zip = info.input_zip
-    data = input_zip.read("IMAGES/" + basename)
+    if firmware:
+        data = input_zip.read("RADIO/" + basename)
+    else:
+        data = input_zip.read("IMAGES/" + basename)
     common.ZipWriteStr(info.output_zip, name, data)
     info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
     info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 
 def OTA_InstallEnd(info, incremental):
-    AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta", incremental)
-    AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo", incremental)
+    AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta", incremental, False)
+    AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo", incremental, False)
+
+def Firmware_Images(info, incremental):
+    """
+    Adds the firmware files from $(INTERNAL_OTA_PACKAGE_TARGET) into the $(LINEAGE_TARGET_PACKAGE).
+    """
+    partition_list = ["lk1", "lk2", "sda", "sdb", "audio_dsp", "cam_vpu1", "cam_vpu2", "cam_vpu3", "gz1", "gz2", "md1img", "oem_misc1", "scp1", "scp2", "spmfw", "sspm_1", "sspm_2", "tee1", "tee2",]
+    for partition in partition_list:
+        AddImage(info, "{}.img".format(partition), "/dev/block/by-name/{}".format(partition), incremental, True)
